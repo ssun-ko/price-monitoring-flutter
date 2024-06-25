f4ac934ce5edf1c42da6280810adcc1d6e5b9c9b
taehyang-kim <taehyang_kim@kolon.com>
2024-06-25 15:01:54 +0900

    Change drawer color to black

--- lib/screens/home/components/side_menu.dart
+++ lib/screens/home/components/side_menu.dart
@@ -11,6 +11,7 @@ class SideMenu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      backgroundColor: Colors.black,
       child: SingleChildScrollView(
         child: Column(
           children: [
