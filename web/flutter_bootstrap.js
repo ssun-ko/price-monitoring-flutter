1f3811a2386a449a58e40de666d99125f1ad46d8
taehyang-kim <taehyang_kim@kolon.com>
2024-07-17 14:07:21 +0900

    Delete loading text

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -3,15 +3,12 @@
 
 const loading = document.createElement('div');
 document.body.appendChild(loading);
-loading.textContent = "Loading Entrypoint...";
 _flutter.loader.load({
   onEntrypointLoaded: async function(engineInitializer) {
-    loading.textContent = "Initializing engine...";
     const appRunner = await engineInitializer.initializeEngine({
         useColorEmoji: true
     });
 
-    loading.textContent = "Running app...";
     await appRunner.runApp();
   }
 });
